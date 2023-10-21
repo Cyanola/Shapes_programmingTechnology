1b1e7db5068488109c18771cd558215f870b2cff
Ольга Артемова <artemovao771@gmail.com>
2023-10-21 17:58:30 +0300

    message

--- src/main/java/Factory/ShapeFactory.java
+++ src/main/java/Factory/ShapeFactory.java
@@ -5,10 +5,27 @@ import javafx.scene.paint.Color;
 import models.* ;
 
 
+import java.util.HashMap;
 import java.util.Objects;
 
 
 public class ShapeFactory implements Creator {
+
+    public ShapeFactory(double x, double y, int line, Color colorborder, Color colorfill, double width, double height){
+        shapes.put("Квадрат", new Square(line, colorborder, colorfill, x, y, width, height));
+        shapes.put("Пятиугольник", new Pentagon(line, colorborder, colorfill, x, y,0,0));
+        shapes.put("Треугольник", new Triangle(line, colorborder, colorfill, x, y,0,0));
+        shapes.put("Угол", new Angle(x,y,line, colorborder, colorfill,0,0));
+        shapes.put("Прямая", new Straight(line, colorborder, colorfill, x, y,0,0));
+        shapes.put("Круг", new Circle(line, colorborder, colorfill, x, y, width, height));
+    }
+    public ShapeFactory()
+    {
+
+    }
+    public HashMap <String, Shape> shapes =new HashMap<String,Shape>(){
+
+    };
     @Override
     public Shape factoryMethod(String name, double x, double y, int line, Color colorborder, Color colorfill, double width, double height ){
         if(Objects.equals(name, "Пятиугольник")){
@@ -23,7 +40,7 @@ public class ShapeFactory implements Creator {
         else if(Objects.equals(name, "Угол")){
             return new Angle(x,y,line, colorborder, colorfill,0,0);
         }
-        else if (Objects.equals(name, "Линия") ||Objects.equals(name, "Прямая")){
+        else if (Objects.equals(name, "Прямая")){
             return new Straight(line, colorborder, colorfill, x, y,0,0);
         }
         else if(Objects.equals(name, "Круг")){
