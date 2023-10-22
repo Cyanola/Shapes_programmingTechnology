0f8cd08a7fa6b51685df1eb82de52d38d917cd85
Ольга Артемова <artemovao771@gmail.com>
2023-10-22 18:26:29 +0300

    dsfgdfhgjh

--- src/main/java/Factory/ShapeFactory.java
+++ src/main/java/Factory/ShapeFactory.java
@@ -10,22 +10,6 @@ import java.util.Objects;
 
 
 public class ShapeFactory implements Creator {
-
-    public ShapeFactory(double x, double y, int line, Color colorborder, Color colorfill, double width, double height){
-        shapes.put("Квадрат", new Square(line, colorborder, colorfill, x, y, width, height));
-        shapes.put("Пятиугольник", new Pentagon(line, colorborder, colorfill, x, y,0,0));
-        shapes.put("Треугольник", new Triangle(line, colorborder, colorfill, x, y,0,0));
-        shapes.put("Угол", new Angle(x,y,line, colorborder, colorfill,0,0));
-        shapes.put("Прямая", new Straight(line, colorborder, colorfill, x, y,0,0));
-        shapes.put("Круг", new Circle(line, colorborder, colorfill, x, y, width, height));
-    }
-    public ShapeFactory()
-    {
-
-    }
-    public HashMap <String, Shape> shapes =new HashMap<String,Shape>(){
-
-    };
     @Override
     public Shape factoryMethod(String name, double x, double y, int line, Color colorborder, Color colorfill, double width, double height ){
         if(Objects.equals(name, "Пятиугольник")){
